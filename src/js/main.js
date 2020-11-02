2dd6add41232d9f0609213f25126731a7fa4b94e
Mohammad Foroutan <mmdfrn81@gmail.com>
2020-11-02 04:33:49 +0330

    chatJs code added for dashboard chart

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3]
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
